308676397a78d2a4669b098d081d277eca864c0a
nujcharee <s_nujcharee@hotmail.com>
2021-04-30 08:38:54 +0100

    Update final.py

--- final.py
+++ final.py
@@ -1,17 +1,56 @@
 class Car(object):
     
-    condition = "New"
+    """
+    Class Car is the parent class.
+    It has a string object "condition" with initial value "New"
     
+    Attributes
+    ----------
+    model  : model of the car e.g Honda, Suzuki, Vauxhall.
+    colour : color of the car e.g Yellow, Blue, White, Black.
+    speed  : speed of car in miles per hours unit.
+    mpg    : miles per gallon.
+    
+    
+    Methods
+    -------
+    display_car
+        Prints a string which reads "This is a [colour] [model] with [mpg] MPG".
+        
+    drive_car
+        Prints a string which reads "used".
+    
+    calculate_stopping_distance
+        Returns a float object which calculates stopping distance.
+    
+    """
+    condition = "New"
     
     def __init__(self, model = None, colour = None, speed = None, mpg=None):
        
+        """ 
+        Constructor that initiate Car class 
+
+        """
+    
         self.model = model
         self.colour = colour
         self.speed = speed
         self.mpg = mpg 
+        
+        
     
     
     def setModel(self, model = None)->str:
+        
+        """
+        Validation rules:
+        ----------------
+
+        * A user can not enter in more than 32 characters
+        * A user can not enter in an empty string or whitespace
+
+        """
         
         while True:
             model = input("Enter model: ")
@@ -20,6 +59,12 @@ class Car(object):
 
                 model == str(model)
                 
+                # Valid conditions
+                if len(model)<=32:
+                    self.model = model
+                    break;
+                
+                # Invalid conditions
                 if not model:
                     print('Please enter car model, dont leave it blank: ')
                     continue
@@ -27,9 +72,6 @@ class Car(object):
                     print("Cant be number.")
                     continue
 
-                if len(model)<=32:
-                    self.model = model
-                    break;
                 if len(model)>32:
                     print("Thats a very long name for a car!")
                     continue
@@ -54,6 +96,7 @@ class Car(object):
                 
                 if colour in colour_list:
                     print("Nice colour!")
+                    self.colour = colour
                     break;    
                 else:
                     print("Choose from following options: ['Yellow', 'Blue', 'Black', 'White', 'Red']")
@@ -63,19 +106,39 @@ class Car(object):
     
     def setSpeed(self, speed=0)->float:
         
+        """
+        Validation rules:
+        ----------------
+
+        * A user can not enter other data type but number.
+        * A user can not enter negative number.
+        * A user can not enter zero.
+        * A user can not enter number greather than 61.
+
+
+        * A user can only enter range from 1-60
+
+        """
+        
         while True:
             speed = input("Enter speeds MPH (between 1-60)): ")
             try:
                 speed = float(speed)
+                
+                # Valid conditions
             
+                if  speed > 0 and speed <= 60 : # normal range https://en.wikipedia.org/wiki/0_to_60_mph
+                    self.speed = speed
+                    break;
+                    
+                # Invalid conditions
+                
                 if speed < 0:
                     print("Speed cannot be negative")
                     continue
                 if speed == 0:
                     print("It appears you aren't moving right now?")
-                if  speed > 0 and speed <= 60 : # normal range https://en.wikipedia.org/wiki/0_to_60_mph
-                    self.speed = speed
-                    break;
+                
                 if 61 <= speed <= 99:
                     print("Impressive speed!!! Normal range is greater than 0 up to 60 (mph).")
                     continue
@@ -95,21 +158,38 @@ class Car(object):
         
     def setMPG(self, mpg=0)->float:
         
+        """
+        Validation rules:
+        ----------------
+
+        * A user can not enter other data type but number.
+        * A user can not enter negative number.
+        * A user can not enter zero.
+        * A user can not enter number greather than 61.
+
+
+        * A user can only enter range from 1-60
+
+        """
+        
         while True:
             mpg = input("Enter MPG (max 60): ")
+            
             try:
                 mpg = float(mpg)
             except ValueError:
                 print('Speed must be number')
                 continue
+            
+            if  mpg > 0 and mpg <= 60 : # normal range https://en.wikipedia.org/wiki/0_to_60_mph
+                self.mpg = mpg
+                break;
+                
             if mpg < 0:
                 print("Speed cannot be negative")
                 continue
             if mpg == 0:
                 print("Car with no MPG information, thats not right?")
-            if  mpg > 0 and mpg <= 60 : # normal range https://en.wikipedia.org/wiki/0_to_60_mph
-                self.mpg = mpg
-                break;
             if 61 <= mpg <= 100:
                 print("Impressive MPG!!! Highest MPG in our record is 60.")
                 continue
@@ -122,7 +202,7 @@ class Car(object):
             else:
                 print("Please retry")
 
-
+    
     def getModel(self):
         return self.model
     
@@ -151,7 +231,7 @@ class Car(object):
         """
         Returns
         -------
-        a string that return the car condition that reads "used".
+        a string that return the car condition that reads "Used".
         
         """
     
@@ -185,12 +265,13 @@ class Car(object):
         self.speed = speed
         
         """
-        tr     : reaction time in seconds (s). The average reaction time of 1.5 (s) is used in the calculations
-        g      : gravitational acceleration, fixed value at 9.81.
+        TR     : reaction time in seconds (s). The average reaction time of 1.5 (s) is used in the calculations
+        G      : gravitational acceleration, fixed value at 9.81.
         
         """
-        self.g = 9.81
-        self.tr = 1.5
+        
+        TR = 1.5
+        G = 9.81
         
         # To convert the speed of the car from miles per hour (mph) 
         # to meter per second (m/s)  
@@ -227,11 +308,17 @@ class Car(object):
         """
         
         mu = self.friction
-        BD = (v_m_s**2)/(2*mu*self.g)
-        RD = v_m_s * self.tr
+        
+        # Braking Distance Calculation
+        BD = (v_m_s**2)/(2*mu*G)
+        
+        # Reaction time in meter per second
+        RD = v_m_s * TR
+        
+        # Stopping Distance Calculation
 
         SD = RD + BD 
-        #return SD
+        # Return SD
         print("\n-----------------------------------")
         print("Stopping distance is {0:.{1}f}".format(SD,2),  " meters.")
         print("-------------------------------------")
@@ -240,12 +327,10 @@ class Car(object):
     def DisplayMenu(self):
         
         print("\n*********************************")
-        print("**********    MENU     **********")
+        print("***** Welcome to OOP Car Sales ****")
         print("*********************************")
-        print("1. Enter Car Information")
-        print("2. Display Car")
-        print("3. Check condition")
-        print("4. Calculate Stopping Distance")
+        print("1. Enter Car Specifications")
+        print("2. View used cars")
         print("Q. Quit")
         print("\nEnter your choice: ", end = "")
     
@@ -254,7 +339,7 @@ class Car(object):
         
         while Choice != "Q":
             self.DisplayMenu()
-            Choice = str(input())
+            Choice = str(input()).upper()
             if Choice == "1":
                 self.setModel()
                 self.setColour()
@@ -262,32 +347,189 @@ class Car(object):
                 self.setMPG() 
                 print("")
                 print("-------------------------------------")
-                print("Car information entered.")
-                print("Select option 2 to review.")
-                print("-------------------------------------")
-            elif Choice == "2":
+                print("Specifications entered: ")
+                print("Thank you!")
                 self.display_car()
-                print("Current condition: ", self.condition)
-                print("-------------------------------------")
-
-            elif Choice == "3":
-                print("-------------------------------------")
-                self.drive_car()
-                print("After condition: ", self.condition)
+                print("Condition: ", self.condition)
                 print("-------------------------------------")
                 
-            elif Choice == "4":
-                print("-------------------------------------")
+                print("Base on given speed, I can calculate stopping distance!")
                 self.calculate_stopping_distance(self.speed)
+                print("-------------------------------------")
 
+            elif Choice == "2":
+                print("-------------------------------------")
+                print("This is a %s %s with %s MPG." % (self.colour, self.model, self.mpg))
+                print("Condition: ", self.drive_car())
+                print("-------------------------------------")
+                
             elif Choice == "Q":
-                print("Simulation finished, press Enter to close.")
+                print("Thank you, come back again soon.")
+                print("press Enter to close.")
                 input()
+            else:
+                print("Only choose from the option above!!!!")
 
+class ElectricCar(Car):
+    
+    """
+    ElectricCar class inherits from Car class.
+    
+    Attributes
+    ----------
+    battery_type  : a string that describes type of battery.
+    
+    
+    Methods
+    -------
+        
+    drive_car
+        Prints a string which reads "less than three years old.".
+    
+    """
+    
+    
+    def __init__(self, model = None, colour = None, speed = None, mpg=None, battery_type=None):
+
+        super().__init__(model, colour, speed, mpg)
+        self._battery_type = battery_type
+    
+       
+    def setBatteryType(self, battery_type=None)->str:
+        
+        
+        battery_list = ["LITHIUM ION BATTERY", "NICKEL-METAL HYDRIDE", "DONT KNOW"]
+        
+        while True:
+        
+            try:
+                battery_type = str(input("Enter battery type: ")).upper()
+                
+                if battery_type in battery_list:
+                    self.battery_type = battery_type
+                    break;    
+                else:
+                    print("[LITHIUM ION BATTERY, NICKEL-METAL HYDRIDE, DONT KNOW]")
+                    print("Choose from these options: ")
+            except ValueError as e:
+                    print("Please retry")
+                    continue
+
+        
+    def getBatteryType(self)->str:
+        return self.battery_type
+  
+    def drive_car(self)->str:
+        
+        Car.drive_car(self)
+        Car.condition = "less than three years old."
+        return Car.condition
+    
+    def RunElectric(self):
+        
+        self.setBatteryType()
+        print("")
+        print("Searching for a car that meets specs.................")
+        Car.display_car(self)
+        print("Battery Type: ", self.getBatteryType())
+        print("Condition: ", self.drive_car())
+
+class PetrolOrDieselCar(Car):
+    """
+    Petrol or Diesel class inherits from Car class.
+    
+    Attributes
+    ----------
+    fuel_type  : a string that describe type of fuel.
+    
+    
+    Methods
+    -------
+        
+    drive_car
+        Prints a string which reads "less than three years old.".
+    
+    """
+    def __init__(self, model = None, colour = None, speed = None, mpg=None, fuel_type=None):
+        super().__init__(model, colour, speed, mpg)
+        self.fuel_type = fuel_type
+
+
+    def setFuelType(self, fuel_type=None):
+        
+        fuel_choice = ['PETROL','DIESEL']
+        
+        while True:
+        
+            try:
+                fuel_type = input("Choose fuel type: ").upper()
+                
+                if fuel_type in fuel_choice:
+                    break;    
+                else:
+                    print("Choose from following options: ['PETROL','DIESEL']")
+            except ValueError as e:
+                    print("Please retry")
+                    continue
+                  
+    
+        self.fuel_type = fuel_type
+        
+        
+    def getFuelType(self):
+        return self.fuel_type
+  
+    def drive_car(self):
+        Car.drive_car(self)
+        Car.condition = "less than three years old."
+        return Car.condition
+    
+    def RunPetrolDiesel(self):
+        self.setFuelType()
+        print("Searching for a car that meets specs.................")
+        print(".....................................................")
+        Car.display_car(self)
+        print("Battery Type: ", self.getFuelType())
+        print("Condition: ", self.drive_car())
+
+def subMenu():
+    
+    print("\n*********************************")
+    print("*****    OOP Car Show Rooms   ****")
+    print("*********************************")
+    print("1. Electric Stocks")
+    print("2. Petrol / Diesel Stocks")
+    print("Q. Quit")
+    print("\nEnter your choice: ", end = "")
+        
 def main():
+    
+    
+    #initialise with default values
 
     mycar = Car("", "", 0, 0)
     mycar.Run()
-
+    
+    print("Before you go, let me show you our car stocks!!!")
+    
+    Choice = ""
+    
+    while Choice != "Q":
+        subMenu()
+        Choice = str(input()).upper()
+        
+        if Choice == "1":
+            eCar = ElectricCar(mycar.getModel(), mycar.getColour(), mycar.getSpeed(), mycar.getMPG(), "")
+            eCar.RunElectric()  
+        elif Choice == "2":
+            pd = PetrolOrDieselCar(mycar.getModel(), mycar.getColour(), mycar.getSpeed(), mycar.getMPG(), "")
+            pd.RunPetrolDiesel()
+        elif Choice == "Q":
+            print("Thank you, come back again soon.")
+            print("press Enter to close.")
+            input()
+        else:
+            print("Only choose from the option above!!!!")
+            
 if __name__ == "__main__":
     main()
